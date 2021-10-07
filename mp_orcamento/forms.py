1cb6cf169b6c4f71033a8cfe729649157e02772c
pliniocefet <plinio.cefet@gmail.com>
2021-10-07 16:20:36 -0300

    testando commit

---
+++ mp_orcamento/forms.py
@@ -0,0 +1,59 @@
+from django.forms import ModelForm
+
+from .models import *
+
+# FORMULARIO DE VENDEDORES
+class FormVendedor(ModelForm):
+    class Meta:
+        model = Vendedor
+        fields = '__all__'
+
+
+# FORMULARIO DE CLIENTE
+class FormCliente(ModelForm):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+
+# FORMULARIO DE LINHA DE PRODUTOS
+class FormLinha(ModelForm):
+    class Meta:
+        model = Linha
+        fields = '__all__'
+
+
+# FORMULARIO DE ALUMINIO
+class FormAluminio(ModelForm):
+    class Meta:
+        model = Aluminio
+        fields = '__all__'
+
+
+# FORMULARIO DE ACESSORIOS
+class FormAcessorio(ModelForm):
+    class Meta:
+        model = Acessorio
+        fields = '__all__'
+
+
+# FORMULARIO DE VIDROS
+class FormVidro(ModelForm):
+    class Meta:
+        model = Vidro
+        fields = '__all__'
+
+
+# FORMULARIO DE PRODUTOS
+class FormProduto(ModelForm):
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+
+# FORMULARIO DE ORÇAMENTOS
+class FormOrcamento(ModelForm):
+    class Meta:
+        pass
+
+
